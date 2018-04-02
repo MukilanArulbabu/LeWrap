45dc3df43945ed19c3881d8cd92fe614bf421e27
Mukilan A <Mukilan_A@external.mckinsey.com>
2018-04-02 20:06:47 +0530

    added footer

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './header';
+import Footer from './footer';
 import Route from '../route';
 import '../styles/custom.scss';
 
@@ -7,6 +8,7 @@ const app = () => (
   <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <Header />
     <Route />
+    <Footer />
   </div>
 );
 
