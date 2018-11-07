c83bc87abea2eeed622b21e617167d0770f5c497
Mukilan A <Mukilan_A@external.mckinsey.com>
2018-11-07 13:33:34 +0530

    pushing sendmail

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const path = require('path');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+
 
 const app = express();
 const PORT = 3000;
 
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'mukilan.arulbabu@gmail.com',
+    pass: 'SAVEE2muki',
+  },
+});
+
+
 app.use(express.static(__dirname));
+app.use(bodyParser.json({
+  limit: '50mb',
+}));
+app.use(bodyParser.urlencoded({
+  limit: '50mb',
+  extended: true,
+}));
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'), (err) => {
     if (err) {
@@ -12,6 +31,28 @@ app.get('/*', (req, res) => {
     }
   });
 });
+
+app.post('/sendMail', (req, res) => {
+  const mailfrom = req.body.email;
+  const subject = `${req.body.firstName} ${req.body.lastName} - ${req.body.mobile}`;
+  const bodyData = req.body.comments;
+  const mailOptions = {
+    from: mailfrom,
+    to: 'sidhu24k@gmail.com',
+    subject,
+    text: bodyData,
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      res.send('error');
+      console.log(error);
+    } else {
+      res.send('sent');
+      console.log(`Email sent: ${info.response}`);
+    }
+  });
+});
+
 
 app.listen(PORT);
 console.log('app is running no issues;');
--- src/components/contactUs.jsx
+++ src/components/contactUs.jsx
@@ -4,7 +4,39 @@ class Contact extends React.Component {
   constructor() {
     super();
     this.state = {};
+    this.sendMail = this.sendMail.bind(this);
   }
+
+  sendMail(e) {
+    e.preventDefault();
+    const fromMail = $('#email').val();
+    const fname = $('#fname').val();
+    const lname = $('#lname').val();
+    const phone = $('#phone').val();
+    const comments = $('#comments').val();
+    const sendData = {
+      email: fromMail,
+      firstName: fname,
+      lastName: lname,
+      mobile: phone,
+      comments,
+    };
+    $.ajax({
+      type: 'POST',
+      url: '/sendMail',
+      data: sendData,
+      dataType: 'text',
+      success(resultData) {
+        console.log(resultData);
+        alert('Save Complete');
+      },
+      failure(err) {
+        console.log(err);
+      },
+    });
+  }
+
+
   render() {
     return (
       <div className="mdl-grid" style={{ width: '100%' }}>
@@ -22,12 +54,15 @@ class Contact extends React.Component {
                   <label htmlFor="lname">Last Name</label>
                   <input type="text" id="lname" name="lastname" placeholder="Your last name.." />
 
+                  <label htmlFor="email">Email Address</label>
+                  <input type="text" id="email" name="email" placeholder="Your email.." />
+
                   <label htmlFor="phone">Mobile</label>
                   <input type="text" id="phone" name="phone" placeholder="Your phone number.." />
 
-                  <label htmlFor="subject">Subject</label>
-                  <textarea id="subject" name="subject" placeholder="Write us something.." style={{ height: 200 }} />
-                  <input type="submit" value="Submit" />
+                  <label htmlFor="comments">Comments</label>
+                  <textarea id="comments" name="comments" placeholder="Write us something.." style={{ height: 200 }} />
+                  <input type="submit" value="Submit" onClick={e => this.sendMail(e)} />
                 </form>
               </div>
             </div>
