9776c510cc1564b7484e6fe09a3cc2ffa76d12b2
MukilanArulbabu <38006716+MukilanArulbabu@users.noreply.github.com>
2020-01-31 17:56:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 3000;
+const PORT = 80;
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
